1466ab823a2fa834bc6a443a47c8acad6aee9fcd
Yerid Stick <yeridstick3000@gmail.com>
2026-02-11 14:44:28 -0500

    update

---
+++ applications/app-service/src/test/java/co/com/bancolombia/Utils/Rule/CleanCodeAttribute.java
@@ -0,0 +1,13 @@
+package co.com.bancolombia.Utils.Rule;
+
+public final class CleanCodeAttribute {
+    public static final co.turismo.Utils.Rule.CleanCodeAttribute EFFICIENT =
+            co.turismo.Utils.Rule.CleanCodeAttribute.EFFICIENT;
+    public static final co.turismo.Utils.Rule.CleanCodeAttribute IDENTIFIABLE =
+            co.turismo.Utils.Rule.CleanCodeAttribute.IDENTIFIABLE;
+    public static final co.turismo.Utils.Rule.CleanCodeAttribute TRUSTWORTHY =
+            co.turismo.Utils.Rule.CleanCodeAttribute.TRUSTWORTHY;
+
+    private CleanCodeAttribute() {
+    }
+}
---
+++ applications/app-service/src/test/java/co/com/bancolombia/Utils/Rule/Severity.java
@@ -0,0 +1,13 @@
+package co.com.bancolombia.Utils.Rule;
+
+public final class Severity {
+    public static final co.turismo.Utils.Rule.Severity HIGH =
+            co.turismo.Utils.Rule.Severity.HIGH;
+    public static final co.turismo.Utils.Rule.Severity LOW =
+            co.turismo.Utils.Rule.Severity.LOW;
+    public static final co.turismo.Utils.Rule.Severity MEDIUM =
+            co.turismo.Utils.Rule.Severity.MEDIUM;
+
+    private Severity() {
+    }
+}
